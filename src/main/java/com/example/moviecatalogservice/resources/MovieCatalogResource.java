39ab3791eb412d5be25fc453e0c2294e7066e27b
PaulZhuzliakov <paulparatrooper@gmail.com>
2021-11-18 00:12:32 +0300

    add RestTemplate

--- src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,20 +1,41 @@
 package com.example.moviecatalogservice.resources;
 
 import com.example.moviecatalogservice.models.CatalogItem;
+import com.example.moviecatalogservice.models.Movie;
+import com.example.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
-    public List<CatalogItem> getCatalog(String userId) {
-        return Collections.singletonList(
-                new CatalogItem("Transformers", "Transformers desc", 4)
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("5586", 1),
+                new Rating("5813", 4)
         );
+
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://127.0.0.1:8082/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Test desc", rating.getRating());
+        })
+                .collect(Collectors.toList());
+
+        //put them all together
     }
+
 }
