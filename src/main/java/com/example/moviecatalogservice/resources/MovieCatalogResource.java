af28f5abb12eef617e3519f5961cd14e0b90fed8
PaulZhuzliakov <paulparatrooper@gmail.com>
2021-11-27 00:41:47 +0300

    make Service a EurekaClient

--- src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -2,19 +2,14 @@ package com.example.moviecatalogservice.resources;
 
 import com.example.moviecatalogservice.models.CatalogItem;
 import com.example.moviecatalogservice.models.Movie;
-import com.example.moviecatalogservice.models.Rating;
 import com.example.moviecatalogservice.models.UserRating;
-import org.apache.catalina.filters.AddDefaultCharsetFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.lang.reflect.ParameterizedType;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,11 +27,13 @@ public class MovieCatalogResource {
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
         UserRating ratings = restTemplate.getForObject(
-                "http://127.0.0.1:8083/ratingsdata/users/" + userId, UserRating.class);
+//                "http://127.0.0.1:8083/ratingsdata/users/" + userId, UserRating.class);
+                "http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
 
         return ratings.getUserRating().stream().map(rating -> {
             //for each movie ID, call movie into service and get details
-            Movie movie = restTemplate.getForObject("http://127.0.0.1:8082/movies/" + rating.getMovieId(), Movie.class);
+//            Movie movie = restTemplate.getForObject("http://127.0.0.1:8082/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
             //put them all together
             return new CatalogItem(movie.getName(), "Test desc", rating.getRating());
         })
